33868ff278abec209a9d3d18e04f975335e9c30d
mavibulut0106@gmail.com <mavibulut0106@gmail.com>
2023-07-23 22:56:27 +0300

    Contact and Explore are Partial Added

---
+++ Casgem_CodeFirstProject/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Casgem_CodeFirstProject.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult PartialSection()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialQuickContact()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialSendMessage()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ Casgem_CodeFirstProject/Controllers/ExploreController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Casgem_CodeFirstProject.Controllers
+{
+    public class ExploreController : Controller
+    {
+        // GET: Explore
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult PartialSection()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialTypesOfTravel()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialTravelRegions()
+        {
+            return PartialView();
+        }
+    }
+}
