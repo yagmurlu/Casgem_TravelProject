4c8c329db41b9492dc45267caf183868ea377f0e
mavibulut0106@gmail.com <mavibulut0106@gmail.com>
2023-07-23 22:25:19 +0300

    About Controller

---
+++ Casgem_CodeFirstProject/Controllers/AboutController.cs
@@ -0,0 +1,35 @@
+﻿using Casgem_CodeFirstProject.DAL.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Casgem_CodeFirstProject.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        TravelContext travelContext = new TravelContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult PartialSection()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialSection1()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialSection2()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialSection3()
+        {
+            return PartialView();
+        }
+    }
+}
