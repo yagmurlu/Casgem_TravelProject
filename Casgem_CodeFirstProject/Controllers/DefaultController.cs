9c11f3cf9fa10fa5dfb1172f58b0840461814919
mavibulut0106@gmail.com <mavibulut0106@gmail.com>
2023-07-24 21:40:49 +0300

    .

--- Casgem_CodeFirstProject/Controllers/DefaultController.cs
+++ Casgem_CodeFirstProject/Controllers/DefaultController.cs
@@ -10,59 +10,114 @@ namespace Casgem_CodeFirstProject.Controllers
 {
     public class DefaultController : Controller
     {
-        // GET: Default
-        TravelContext travelContext=new TravelContext();
+        TravelContext travelContext = new TravelContext();
         public ActionResult Index()
         {
             return View();
         }
-       
+
         public PartialViewResult PartialHead()
         {
             return PartialView();
         }
-        public PartialViewResult PartialBooking()
-        {
-           
-            return PartialView();
-        }
+
         public PartialViewResult PartialNavbar()
         {
+            ViewBag.phone = travelContext.FooterContacts.Select(x => x.Phone).FirstOrDefault();
+
             return PartialView();
         }
-        public PartialViewResult PartialSliderScript()
+
+        public PartialViewResult PartialNavbarScript()
         {
             return PartialView();
         }
-        public PartialViewResult PartialSlider()
-        {
-            var values = travelContext.Sliders.ToList();
-            return PartialView(values);
-        }
-       
+
         public PartialViewResult PartialFeature()
         {
+            ViewBag.title = travelContext.Abouts.Select(x => x.Title).FirstOrDefault();
+            ViewBag.description = travelContext.Abouts.Select(x => x.Description).FirstOrDefault();
+
             return PartialView();
+        }
+
+        [HttpGet]
+        public PartialViewResult PartialHolidayPlan()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public PartialViewResult PartialHolidayPlan(Booking p)
+        {
+            travelContext.Bookings.Add(p);
+            travelContext.SaveChanges();
+            return PartialView();
+        }
+        public PartialViewResult PartialPlace()
+        {
+            var values = travelContext.Places.ToList();
+            return PartialView(values);
         }
         public PartialViewResult PartialDestination()
         {
             var values = travelContext.Destinations.ToList();
             return PartialView(values);
         }
-        public PartialViewResult PartialAbout()
+        public PartialViewResult PartialExplore()
         {
+            ViewBag.title = travelContext.Explores.Select(x => x.Title).FirstOrDefault();
+            ViewBag.description = travelContext.Explores.Select(x => x.Description).FirstOrDefault();
+            ViewBag.imageUrl = travelContext.Explores.Select(x => x.ImageUrl).FirstOrDefault();
             return PartialView();
         }
-        public PartialViewResult PartialBookingCover()
-        {
-            return PartialView();
-        }
-        public PartialViewResult PartialFooter()
+
+        public PartialViewResult PartialBookTravel()
         {
             return PartialView();
         }
 
-        public PartialViewResult PartialMainScript()
+        public PartialViewResult PartialFooter()
+        {
+            ViewBag.address = travelContext.FooterContacts.Select(x => x.Address).FirstOrDefault();
+            ViewBag.phone = travelContext.FooterContacts.Select(x => x.Phone).FirstOrDefault();
+            ViewBag.mail = travelContext.FooterContacts.Select(x => x.Mail).FirstOrDefault();
+            return PartialView();
+        }
+
+        public PartialViewResult PartialSocialMedia()
+        {
+            var values = travelContext.SocialMedias.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialCategory()
+        {
+            var values = travelContext.Categories.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialGalery()
+        {
+            var values = travelContext.FooterGaleries.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialTravelTrip()
+        {
+            var values = travelContext.FooterTravelTrips.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
+        public PartialViewResult Team()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialFeature2()
         {
             return PartialView();
         }
