698db93b2661975bf00d364ab389988a88208555
mavibulut0106@gmail.com <mavibulut0106@gmail.com>
2023-07-26 23:48:10 +0300

    Explore Completed

--- Casgem_CodeFirstProject/Controllers/ExploreController.cs
+++ Casgem_CodeFirstProject/Controllers/ExploreController.cs
@@ -1,4 +1,5 @@
 ﻿using Casgem_CodeFirstProject.DAL.Context;
+using Casgem_CodeFirstProject.DAL.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,48 @@ namespace Casgem_CodeFirstProject.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+      
+        public ActionResult GetImage()
+        {
+            var values = travelContext.Galeries.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult EditImage(int id)
+        {
+            var value = travelContext.Galeries.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult EditImage(Galery galery)
+        {
+            var value = travelContext.Galeries.Find(galery.GaleryId);
+            value.Title = galery.Title;
+            value.Description = galery.Description;
+            value.ImageUrl = galery.ImageUrl;
+
+            travelContext.SaveChanges();
+            return RedirectToAction("GetImage");
+        }
+        [HttpGet]
+        public ActionResult AddImage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddImage(Galery galery)
+        {
+            travelContext.Galeries.Add(galery);
+            travelContext.SaveChanges();
+            return RedirectToAction("GetImage");
+        }
+        public ActionResult DeleteImage(int id)
+        {
+            var value = travelContext.Galeries.Find(id);
+            travelContext.Galeries.Remove(value);
+            travelContext.SaveChanges();
+            return RedirectToAction("GetImage");
         }
 
         public PartialViewResult PartialService()
